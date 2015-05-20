306194995f0827ecd87a6366214620d143529b10
Suhail Abood <suhail.abood@gmail.com>
2015-05-20 01:07:27 +0100

    basic implementation

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-# project-genie
+# Project Genie
 
-A project generator npm package that makes it easy to generate project folder structure, download libraries, create gulpfile.js or Gruntfile.js, etc.  
+Project Genie is a project generator module that makes it easy to generate project folder structure, download libraries, create automation files i.e. gulpfile.js or Gruntfile.js, etc.  
 
 ## Usage
 
-Clone the GitHub repo:
+### Install using NPM 
+```sh
+	npm install project-genie --save-dev
+```
+
+### Clone the GitHub repo:
 
 ```sh
     git clone https://github.com/suhdev/project-genie.git
@@ -12,6 +17,7 @@ Clone the GitHub repo:
 
 This is still a work in progress. 
 
+### Usage
 
 ### Copyright
 Copyright (c) 2015 Suhail Abood
--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-var Generator = require('./generator'),
-	FolderTask = require('./tasks/folder'),
-	GulpTask = require('./tasks/gulp'),
-	BowerTask = require('./tasks/bower');
+/** 
+ * @namespace ProjectGenie
+ * @description
+ * Project Genie Module
+ */
+var generator = require('./src/generators/basic'),
+	task = require('./src/tasks/basic'),
+	folderTask = require('./src/tasks/folder'),
+	gulpTask = require('./src/tasks/gulp'),
+	npmTask = require('./src/tasks/npm'),
+	bowerTask = require('./src/tasks/bower');
 	
- var basicGen = new Generator();
- basicGen.register(new BowerTask());
- basicGen.register(new FolderTask());
- basicGen.register(new GulpTask());
- basicGen.next();
+ // var basicGen = new Generator();
+ // basicGen.register(new BowerTask());
+ // basicGen.register(new FolderTask());
+ // basicGen.register(new GulpTask());
+ // basicGen.next();
+
+
+module.exports = { 
+	Generator:generator,
+	Task:task,
+	/**
+	 * @namespace ProjectGenie.Tasks
+	 * @name ProjectGenie.Tasks
+	 * @description
+	 * Contains implementations of a set of basic tasks 
+	 */
+	Tasks:{
+		NpmTask:npmTask,
+		BowerTask:bowerTask,
+		GulpTask:gulpTask,
+		FolderTask:folderTask
+	}
+};
